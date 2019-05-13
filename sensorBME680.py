bf278c1e9661b0fec3886c2090b04a207c79ac5d
Runkunkun <lukas98kurth@protonmail.com>
2019-05-14 01:54:25 +0200

    fixed mistake in else clause at air quality calculation

--- sensorBME680.py
+++ sensorBME680.py
@@ -54,14 +54,14 @@ def update_air_quality():
             hum_score = (hum / hum_baseline)
             hum_score *= (0.25 * 100)
         else:
-            hum_score = 0.25
+            hum_score = 25
 
     if gas_offset > 0:
         gas_score = (gas / gas_baseline)
         gas_score *= (0.75 * 100)
 
     else:
-        gas_score = 0.75
+        gas_score = 75
 
     air_quality_score = hum_score + gas_score
     return air_quality_score
